9a62fffb057d678a640e747f7d6e6649fd1f233d
bittiboop <sstrelkova15@gmail.com>
2024-04-29 23:09:55 +0300

    added ex 1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main() {
+	float num0, num1, num2, num3;
 
-
+	cin >> num1 >> num2 >> num3;
+	num0 = 1 / num1 + 1 / num2 + 1 / num3;
+	cout << "Result:" << 1 / num0 << endl;
 	return 0;
 }
